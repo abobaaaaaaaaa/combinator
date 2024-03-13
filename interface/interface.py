c8ce5064770d4e44865a04285d8c265c783a530f
Artem457100 <151593648+Artem457100@users.noreply.github.com>
2024-03-13 11:50:47 +0700

    Update interface.py

--- interface/interface.py
+++ interface/interface.py
@@ -5,8 +5,8 @@ from math import *
 from itertools import combinations
 import itertools
 
-def ya_ne_znau_chto_pridymatb(ASSS,asS,f):
-    po = len(ASSS.get().split())
+def ya_ne_znau_chto_pridymatb(hot,asS,f):
+    po = len(hot.get().split())
     d = asS.get()
     ASs = Label(f, text=f"{po ** d}")
     ASs.pack()
@@ -16,41 +16,42 @@ def permutations_with_repetition(n, k_list):
     for k in k_list:
         denominator *= factorial(k)
     return factorial(n) // denominator
+
 def comb_rep(elem,r):
-    res = itertools.combinations_with_replacement(elem, r)
+    res=itertools.combinations_with_replacement(elem, r)
     return len(res)
+
 def comb(elem):
     res = combinations(elem)
     return len(res)
-def razmeshenia_c_povtoreniami():
+
+def perestanovki_s_povt():
     f = Tk()
-    ASSS = ttk.Entry(f)
-    ASSS.pack()
-    asS = ttk.Entry(f)
-    asS.pack()
-    qwerty = ttk.Button(f, text ="расчитать количество размещений с повоторениями", command=lambda: ya_ne_znau_chto_pridymatb(ASSS,asS,f))
+    Entry1 = ttk.Entry(f)
+    Entry1.pack()
+    Entry2 = ttk.Entry(f)
+    Entry2.pack()
+    qwerty = ttk.Button(f, text ="Нажми для вывода результата", command=lambda: ya_ne_znau_chto_pridymatb(Entry1,Entry2,f))
     qwerty.pack()
 
-
-
-def rasmeshenia():
+def razmeshenia():
     m = Tk()
     a = ttk.Entry(m)
     a.pack(anchor=NW, padx=8, pady=8)
     k = ttk.Entry(m)
     k.pack(anchor=NW, padx=8, pady=8)
 
-    def IDITE_NAFIG_CO_CBOIMI_NAZBANIAMI():
+    def BUTton_create():
         c = a.get().split()
         d = int(k.get())
         f = Label(m, text=f"{len(c) ** d}")
         f.pack()
 
-    h = ttk.Button(m, text="hgtubgthufnr3", command=IDITE_NAFIG_CO_CBOIMI_NAZBANIAMI)
+    h = ttk.Button(m, text="Нажми для вывода результата", command=BUTton_create)
     h.pack(anchor=NW, padx=8, pady=8)
 
 
-def bruh():
+def perestanovki():
     m = Tk()
     a = ttk.Entry(m)
     a.pack(anchor=NW, padx=8, pady=8)
@@ -60,11 +61,11 @@ def bruh():
         b = Label(m, text=f"{factorial(len(c))}")
         b.pack()
 
-    calculate_button = ttk.Button(m, text="рассчитать количество перестановок", command=calculate_factorial)
+    calculate_button = ttk.Button(m, text="Нажми для вывода результата", command=calculate_factorial)
     calculate_button.pack()
 
     m.mainloop()
-def perestanovki_c_povtoreniami():
+def igfnjunu():
     kl = Tk()
     a = ttk.Entry(kl)
     a.pack(anchor=NW, padx=8, pady=8)
@@ -78,31 +79,27 @@ def perestanovki_c_povtoreniami():
         ui = Label(kl, text=f"{permutations_with_repetition(len(c),d)}")
         ui.pack()
 
-    calculate_button = ttk.Button(kl, text="рассчитать количество перестановок", command=hfuihfewu)
+    calculate_button = ttk.Button(kl, text="Нажми для вывода результата", command=hfuihfewu)
     calculate_button.pack()
-
     kl.mainloop()
+
 def ryd(a,kop,j):
         b = len(a.get().split())
         k = int(kop.get())
-        ass = Label(j, text=f"{comb(b, k)}")
-        ass.pack()
-def cochetania_bez_povtoreniy():
+        Lavel = Label(j, text=f"{comb(b, k)}")
+        Lavel.pack()
+
+def sochetania_s_povt():
     j = Tk()
     a = ttk.Entry(j)
     a.pack(anchor=NW, padx=8, pady=8)
     kop = ttk.Entry(j)
     kop.pack()
-
-
-
-    d = ttk.Button(j, text="посчитать сочетания", command= lambda:ryd(a,kop,j))
+    d = ttk.Button(j, text="Нажми для вывода результата", command= lambda:ryd(a,kop,j))
     d.pack()
     j.mainloop()
 
-
-
-def cochetania_c_povtoreniami():
+def sochetania_bez_povt():
     j = Tk()
     a = ttk.Entry(j)
     a.pack(anchor=NW, padx=8, pady=8)
@@ -114,42 +111,23 @@ def cochetania_c_povtoreniami():
         ass = Label(j, text=f"{comb_rep(b, int(kop.get()))}")
         ass.pack(anchor=CENTER)
 
-    d = ttk.Button(j, text="посчитать сочетания", command=ryd)
+    d = ttk.Button(j, text="Нажми для вывода результата", command=ryd)
     d.pack()
-def CUMozin_2():
-    m = Tk()
-    a = ttk.Entry(m)
-    a.pack(anchor=NW, padx=8, pady=8)
-    k = ttk.Entry(m)
-    k.pack(anchor=NW, padx=8, pady=8)
 
-    def IDITE_NAFIG_CO_CBOIMI_NAZBANIAMI():
-        c = a.get().split()
-        d = int(k.get())
-        u = factorial(len(c))
-        gf =  factorial( len(c) - d)
-        f = Label(m, text=f"{u / gf}")
-        f.pack()
-
-    h = ttk.Button(m, text="hgtubgthufnr3", command=IDITE_NAFIG_CO_CBOIMI_NAZBANIAMI)
+    h = ttk.Button(m, text="рассчитать", command=Button_CREate)
     h.pack(anchor=NW, padx=8, pady=8)
-
 
 root = Tk()
 root.title("Калькулятор")
 root.geometry("300x250")
-
-ab = ttk.Button(root, text="перестановки", command=bruh)
-ab.pack()
-af = ttk.Button(root, text="перестановки с повторениями", command=perestanovki_c_povtoreniami)
-af.pack()
-ac = ttk.Button(root, text="размещения", command=rasmeshenia)
-ac.pack()
-ASS = ttk.Button(text="размещения c повторениями", command=razmeshenia_c_povtoreniami)
-ASS.pack()
-ass = ttk.Button(root, text="сочетания без повторений", command=cochetania_bez_povtoreniy)
-ass.pack()
-Ass = ttk.Button(root, text="сочетания с повторениями", command=cochetania_c_povtoreniami)
-Ass.pack()
-
+button4 = ttk.Button(root, text="перестановки", command=perestanovki)
+button4.pack()
+button3 = ttk.Button(root, text="размещения", command=razmeshenia)
+button3.pack()
+button = ttk.Button(text="размещения c повторениями", command=perestanovki_s_povt)
+button.pack()
+button2 = ttk.Button(root, text="сочетания с повторениями", command=sochetania_s_povt)
+button2.pack()
+button1 = ttk.Button(root, text="сочетания без повторений", command=sochetania_bez_povt)
+button1.pack()
 root.mainloop()
